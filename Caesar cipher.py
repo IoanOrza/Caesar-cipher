091ff714700d6ddc0dcb9ce3b34f45d73ecce229
IoanOrza <100422510+IoanOrza@users.noreply.github.com>
2022-03-07 09:30:18 +0200

    Update Caesar cipher.py

--- Caesar cipher.py
+++ Caesar cipher.py
@@ -1,28 +1,3 @@
-# # Ceasar cipher pentru un cuvant
-
-# while True:
-#     try:
-#         cuv = input('Introduceti cuvantul care urmeaza sa fie criptat: ')
-
-#         cheie = int(input('Introduceti un numar intreg, care va fi cheia cifrului: '))
-
-#         break
-
-#     except:
-#         print('Date de intrare invalide!')
-
-# # transformam toate literele cuvantului in majuscule
-# cuv = cuv.upper()
-
-# l = len(cuv) # memoram lungimea cuvantului intr-o  variabila
-# cuv2 = ''
-
-# for i in range(l):
-#     cod = 65 + (ord(cuv[i]) - 65 + cheie) % 26
-#     cuv2 += chr(cod)
-
-# print(cuv2)
-    
 # Caesar cipher pentru o fraza
 
 # introducerea de catre utilizator a datelor de intrare
